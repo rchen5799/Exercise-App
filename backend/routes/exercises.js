6a3eec60e12fc5461086d577df95381ed84eab56
rchen5799 <rchen5799@Gmail.com>
2020-10-07 23:42:22 -0400

    Added more functions for exercise database

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -28,4 +28,38 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+//:id is an object id created automatically by MongoDB
+//This is getting id directly from URL
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//like above, except for delete request
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted'))
+        .catch(err => res.status(400).json('Error ' + err));
+});
+
+//Update function
+//This route needs username, description, duration, date
+router.route('/update/:id').post((req, rest) => {
+    //Need to find the current exercise via id
+    Exercise.findById(req.params.id)
+        //After we get it, update the variables
+        .then(exercse => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date)
+
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error ' + err));
+})
+
 module.exports = router;
