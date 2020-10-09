c849e644a26bc0f228ae9ef7cbe0acaea433ccce
rchen5799 <rchen5799@Gmail.com>
2020-10-08 23:56:09 -0400

    Starting Frontend React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+//For customized css
+//import './index.css';
+
+//Where we create front-end react app
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -8,10 +11,12 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  //loaded here (see index.html)
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//We're not going to use ServiceWorker here
+// serviceWorker.unregister();
