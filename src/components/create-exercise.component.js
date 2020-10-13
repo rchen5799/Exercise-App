fcc55074380b58140caa06e6375905be81cebbac
rchen5799 <rchen5799@Gmail.com>
2020-10-12 23:29:06 -0400

    component editing

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class ExerciseList extends Component {
+export default class CreateExercises extends Component {
     render() {
         return (
             <div>
-                <p>You are on the Exercises List Component!</p>
+                <p>You are on the Create Exercises Component!</p>
             </div>
         )
     }
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-export default class ExerciseList extends Component {
+export default class CreateUsers extends Component {
     render() {
         return (
             <div>
-                <p>You are on the Exercises List Component!</p>
+                <p>You are on the Create Users Component!</p>
             </div>
         )
     }
