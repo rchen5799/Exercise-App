310f587bc69b8e84e7b25ff13e7b5e54e4800579
rchen5799 <rchen5799@Gmail.com>
2020-10-26 23:43:31 -0400

    some css bugs :(

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css'
 //import logo from './logo.svg';
 //import './App.css';
 
