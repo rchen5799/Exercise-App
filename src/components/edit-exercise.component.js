4a477ee7a75adb43912344e74d1b5f0733efa495
rchen5799 <rchen5799@Gmail.com>
2020-10-30 00:07:42 -0400

    edit exer

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -155,41 +155,39 @@ export default class EditExercise extends Component {
                             }
                         </select>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group"> 
                         <label>Description: </label>
-                        <input type="text"
+                        <input  type="text"
                             required
                             className="form-control"
                             value={this.state.description}
-                            onChange={this.onChangeDescription} 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Duration (in minutes): </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={this.state.duration}
-                            onChange={this.onChangeDuration} 
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label>Date: </label>
-                        <div>
-                            <DatePicker
-                                select={this.state.date}
-                                onChange={this.onChangeDate}
+                            onChange={this.onChangeDescription}
                             />
-                        </div>
+                    </div>
+                    <div className="form-group">
+                    <label>Duration (in minutes): </label>
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        value={this.state.duration}
+                        onChange={this.onChangeDuration}
+                        />
+                    </div>
+                    <div className="form-group">
+                    <label>Date: </label>
+                    <div>
+                        <DatePicker
+                        selected={this.state.date}
+                        onChange={this.onChangeDate}
+                        />
+                    </div>
                     </div>
 
                     <div className="form-group">
-                        <input type="submit"
-                            value="Edit Exercise Log"
-                            className="btn btn-primary" />
+                    <input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
                     </div>
                 </form>
-            </div>
+                </div>
         )
     }
 }
