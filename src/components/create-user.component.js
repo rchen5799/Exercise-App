bd49fbbdacc72aa27f19c0a8a834a30db3a876cd
rchen5799 <rchen5799@Gmail.com>
2020-10-31 23:51:04 -0400

    deeebbbuggg

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -59,23 +59,23 @@ export default class CreateUsers extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Create New User</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Username: </label>
-                        <input type="text"
-                            required
-                            className="form-control"
-                            value={this.state.username}
-                            onChange={this.onChangeUsername}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Create User" className="btn brn-primary" />
-                    </div>
-                </form>
-            </div>
+          <div>
+            <h3>Create New User</h3>
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group"> 
+                <label>Username: </label>
+                <input  type="text"
+                    required
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChangeUsername}
+                    />
+              </div>
+              <div className="form-group">
+                <input type="submit" value="Create User" className="btn btn-primary" />
+              </div>
+            </form>
+          </div>
         )
+      }
     }
-}
